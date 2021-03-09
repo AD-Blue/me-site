25fa4ec7da76c3ba61f73659895e48290d030c1b
The-Real-Guy <aasdimaranan@gmail.com>
2021-03-09 02:10:46 -0700

    Added Socials images

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
-import { Flex, Box, Heading, Text } from '@chakra-ui/react';
+import { Flex, Box, Heading, Text, Link } from '@chakra-ui/react';
 import SkillList from '../components/SkillList';
 import styles from '../styles/Home.module.css';
 import ProjectCard from '../components/ProjectCard';
+import Image from 'next/image';
 
 export default function Home() {
     return (
@@ -13,7 +14,7 @@ export default function Home() {
             <Flex
                 className={styles.main}
                 direction='column'
-                h='550vh'
+                h='650vh'
                 alignContent='center'
             >
                 <Box textAlign="center" mt='20%'>
@@ -24,7 +25,7 @@ export default function Home() {
                         Full-Stack Developer
                     </Heading>
                 </Box>
-                <Flex direction='column' mt='25%'>
+                <Flex direction='column' mt='25%' ml='5%'>
                     <Heading as='h3' size='xl' color='#ccc' fontSize='54px'>
                         Hi There
                     </Heading>
@@ -42,7 +43,7 @@ export default function Home() {
                     <Heading as='h3' size='xl' fontSize='54px' color='#ccc' mt='7%' mb='5%'>
                         The Things I've Made
                     </Heading>
-                    <Flex align='center' direction='column'>
+                    <Flex align='center' direction='column' mr='5%'>
                         <Heading as='h4' size='lg' color='#ccc' mb='2%'>Coffee Till Point-of-Sale</Heading>
                         <ProjectCard img="/images/coffeetill.PNG" url="https://coffee-till.netlify.app/" />
                         <Heading as='h4' size='lg' color='#ccc' mb='2%' mt='5%'>Cookbook</Heading>
@@ -50,6 +51,21 @@ export default function Home() {
                         <Heading as='h4' size='lg' color='#ccc' mb='2%' mt='5%'>Zapr Blogs</Heading>
                         <ProjectCard img="/images/zapr.PNG" url="https://zapr.netlify.app/" />
                     </Flex>
+
+                    <Heading as='h3' size='xl' fontSize='54px' color='#ccc' mt='7%' mb='5%'>
+                        Know What You Want to Make? Let's Talk
+                    </Heading>
+                    <Flex direction='row'>
+                        <Flex direction='column' justify='space-between' h='300px'>
+                            <Link href='https://www.github.com/AD-Blue' isExternal='true'>
+                                <Image src='/images/github.png' height={120} width={120} />
+                            </Link>
+                            <Link href='https://www.linkedin.com/in/aries-dimaranan-5664b91a1/' isExternal='true'>
+                                <Image src='/images/linkedin.png' height={100} width={120} />
+                            </Link>
+                        </Flex>
+                    </Flex>
+
                 </Flex>
             </Flex>
         </>
