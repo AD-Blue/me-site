0b8af42f749adfd4f0be782f8d1392fbe867905e
The-Real-Guy <aasdimaranan@gmail.com>
2021-03-14 01:43:33 -0700

    Solution confirmed, removing script tag for live site

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,6 @@ export default function Home() {
         <Box w='100%'>
             <Head>
                 <title>Aries Dev</title>
-                <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/19505208.js"></script>
             </Head>
             <Flex
                 direction='column'
